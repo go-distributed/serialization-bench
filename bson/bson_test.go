a206e3b4d09a5d8e6e326e714180e361e8bd2a97
Hongchao Deng <fengjingchao@gmail.com>
2013-12-15 16:34:28 -0500

    finish bson benchmark

---
+++ bson/bson_test.go
@@ -0,0 +1,31 @@
+package bson
+
+import (
+	"fmt"
+	"testing"
+
+	"labix.org/v2/mgo/bson"
+)
+
+var _ = fmt.Printf
+
+type Data struct {
+	N int
+	S string
+	A []byte
+}
+
+func BenchmarkBson(b *testing.B) {
+	length := 100
+	a := make([]byte, length, length)
+
+	putData := &Data{A: a}
+	getData := &Data{}
+
+	for i := 0; i < b.N; i++ {
+		putData.N = i
+		putData.S = string(i)
+		data, _ := bson.Marshal(putData)
+		bson.Unmarshal(data, getData)
+	}
+}
