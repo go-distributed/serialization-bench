84a9037d3f75ce253f9600e6550db9feec76c74c
Xiang Li <xiang.li@coreos.com>
2013-12-16 12:51:29 -0800

    finish capnp bench

--- capnproto/capnp_test.go
+++ capnproto/capnp_test.go
@@ -7,6 +7,12 @@ import (
 	"github.com/jmckaskill/go-capnproto"
 )
 
+type Data struct {
+	N int32
+	S string
+	A []byte
+}
+
 func TestCapnproto(t *testing.T) {
 	// Write
 	s := capn.NewBuffer(nil)
@@ -27,4 +33,48 @@ func TestCapnproto(t *testing.T) {
 	if st.N() != 1 || st.S() != "hello world" || string(st.A()) != "hello world" {
 		t.Fatal("error on decoding capnproto")
 	}
+}
+
+func BenchmarkCapnprotoMarshal(b *testing.B) {
+	length := 100
+	a := make([]byte, length, length)
+	buf := bytes.Buffer{}
+
+	putData := Data{N: 1, S: "abc", A: a}
+
+	for i := 0; i < b.N; i++ {
+		s := capn.NewBuffer(nil)
+		st := NewRootTestST(s)
+		st.SetN(putData.N)
+		st.SetS(putData.S)
+		st.SetA(putData.A)
+		s.WriteTo(&buf)
+		buf.Reset()
+	}
+}
+
+func BenchmarkCapnprotoUnmarshal(b *testing.B) {
+	length := 100
+	a := make([]byte, length, length)
+	buf := bytes.Buffer{}
+
+	putData := Data{N: 1, S: "abc", A: a}
+	s := capn.NewBuffer(nil)
+	st := NewRootTestST(s)
+	st.SetN(putData.N)
+	st.SetS(putData.S)
+	st.SetA(putData.A)
+	s.WriteTo(&buf)
+
+	for i := 0; i < b.N; i++ {
+		s, _, err := capn.ReadFromMemoryZeroCopy(buf.Bytes())
+		if err != nil {
+			panic(i)
+		}
+		st = ReadRootTestST(s)
+		if st.N() != 1 || st.S() != "abc" {
+			panic(i)
+		}
+	}
+
 }
