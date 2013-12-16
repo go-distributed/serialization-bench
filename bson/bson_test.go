f03151a55681cc8c95f9d4e14c221d556db7c5c3
Hongchao Deng <fengjingchao@gmail.com>
2013-12-16 14:54:01 -0500

    finish gobin codegen benchmark

--- bson/bson_test.go
+++ bson/bson_test.go
@@ -15,6 +15,7 @@ type Data struct {
         A       []byte
 }
 
+
 func BenchmarkBsonMarshal(b *testing.B) {
         length := 100
         a := make([]byte, length, length)
@@ -36,6 +37,41 @@ func BenchmarkBsonUnmarshal(b *testing.B) {
         putData.N = 1
         putData.S = "abc"
         getData := &Data{}
+
+        data, _ := bson.Marshal(putData)
+
+        for i := 0; i < b.N; i++ {
+                bson.Unmarshal(data, getData)
+        }
+}
+
+type Data2 struct {
+        N       int
+        S       []byte
+        A       []byte
+}
+
+func BenchmarkMarsNoS(b *testing.B) {
+        length := 100
+        a := make([]byte, length, length)
+
+        putData := &Data2{A: a}
+        putData.N = 1
+        putData.S = []byte{'a', 'b', 'c'}
+
+        for i := 0; i < b.N; i++ {
+                bson.Marshal(putData)
+        }
+}
+
+func BenchmarkUnmaNoS(b *testing.B) {
+        length := 100
+        a := make([]byte, length, length)
+
+        putData := &Data2{A: a}
+        putData.N = 1
+        putData.S = []byte{'a', 'b', 'c'}
+        getData := &Data2{}
 
         data, _ := bson.Marshal(putData)
 
